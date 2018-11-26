5756521dde027fb3b976951ad7eab1eb0d3c7c60
Fatcat <root@127.0.0.1>
2018-11-26 20:12:26 +0000

    changes to examples

--- examples/yash_config_examples.rb
+++ examples/yash_config_examples.rb
@@ -17,20 +17,18 @@ config.each do |key, value|
 end
 
 puts File.open(config[:config_file]).readlines
-
 puts "Original value: " + config[:string_value]
 config[:string_value] = 'one'
 puts "Changed value: " + config[:string_value]
-
+puts
 puts "Add a new configuration parameter"
 config[:new_param] = 'new'
-
 puts File.open(config[:config_file]).readlines
-
-#puts "Clear configuration"
-#config.clear_configuration
-
-#puts File.open(config[:config_file]).readlines
-
-#config[:config_file] = "#{ENV['HOME']}/new-config.yaml"
-#puts File.open(config[:config_file]).readlines
+puts
+puts "Clear configuration"
+config.clear_configuration
+puts File.open(config[:config_file]).readlines
+puts
+puts "Change filename"
+config[:config_file] = "#{ENV['HOME']}/new-config.yaml"
+puts File.open(config[:config_file]).readlines
